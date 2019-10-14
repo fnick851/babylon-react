d9cccd69d7f72016d91036193928b6bb8c377d4d
Noah <hanlun.song@gmail.com>
2019-10-13 21:14:47 -0400

    better types

--- src/App.tsx
+++ src/App.tsx
@@ -3,56 +3,37 @@ import * as BABYLON from "babylonjs"
 import "./App.css"
 
 const App: React.FC = () => {
-    const canvasEl = useRef(null)
+    const canvasEl = useRef<HTMLCanvasElement>(null)
 
     useEffect(() => {
-        const engine = new BABYLON.Engine(canvasEl.current, true) // Generate the BABYLON 3D engine
+        // Generate the BABYLON 3D engine
+        const engine = new BABYLON.Engine(canvasEl.current, true)
+        // Create the scene space
+        const scene = new BABYLON.Scene(engine)
 
-        /******* Add the create scene function ******/
-        const createScene = () => {
-            // Create the scene space
-            const scene = new BABYLON.Scene(engine)
-
-            // Add a camera to the scene and attach it to the canvas
-            const camera = new BABYLON.ArcRotateCamera(
-                "Camera",
-                Math.PI / 2,
-                Math.PI / 2,
-                2,
-                new BABYLON.Vector3(0, 0, 5),
-                scene
-            )
-            camera.attachControl(
-                (canvasEl.current as unknown) as HTMLElement,
-                true
-            )
-
-            // Add lights to the scene
-            new BABYLON.HemisphericLight(
-                "light1",
-                new BABYLON.Vector3(1, 1, 0),
-                scene
-            )
-            new BABYLON.PointLight(
-                "light2",
-                new BABYLON.Vector3(0, 1, -1),
-                scene
-            )
-
-            // Add and manipulate meshes in the scene
-            BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene)
-
-            return scene
-        }
-        /******* End of the create scene function ******/
-
-        var scene = createScene() //Call the createScene function
+        // Add a camera to the scene and attach it to the canvas
+        new BABYLON.ArcRotateCamera(
+            "Camera",
+            Math.PI / 2,
+            Math.PI / 2,
+            2,
+            new BABYLON.Vector3(0, 0, 5),
+            scene
+        ).attachControl(canvasEl.current as HTMLCanvasElement, true)
+        // Add lights to the scene
+        new BABYLON.HemisphericLight(
+            "light1",
+            new BABYLON.Vector3(1, 1, 0),
+            scene
+        )
+        new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene)
+        // Add and manipulate meshes in the scene
+        BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene)
 
         // Register a render loop to repeatedly render the scene
         engine.runRenderLoop(function() {
             scene.render()
         })
-
         // Watch for browser/canvas resize events
         window.addEventListener("resize", function() {
             engine.resize()
